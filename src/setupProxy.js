3f80630b9073cc5929e5527636ca8fe645f15f01
sysdsoft\xiaojie <xiaojie@sysdsoft.cn>
2024-09-20 10:06:11 +0800

    添加环境变量

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('/api', {
-            target: 'http://localhost:5001/',
+        createProxyMiddleware(process.env.REACT_APP_BASE_URL, {
+            target: process.env.REACT_APP_API_URL,
             changeOrigin: true,
             pathRewrite: {
-                '^/api': '',
+                [`^${process.env.REACT_APP_BASE_URL}`]: '',
             },
         })
     );
